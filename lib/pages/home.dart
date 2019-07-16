304de6686df6566f30dc1165f1d5200966aec654
WangMingwei <wangmingwei623@163.com>
2019-07-16 15:40:05 +0800

    animation run

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,8 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutterdemo/constant/color.dart';
 import 'package:flutter/animation.dart';
+import 'dart:math' as math;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -13,56 +13,88 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   Offset _offset = Offset.zero;
   Animation<double> animation;
   AnimationController animationController;
+  double offsetStartX = 1.0;
+  bool rotateSign = true;
 
   @override
   void initState() {
     super.initState();
     animationController = new AnimationController(
-        duration: const Duration(milliseconds: 2000), vsync: this);
+        duration: const Duration(milliseconds: 1000), vsync: this);
 //    animation = new CurvedAnimation(parent: animationController, curve: Curves.fastOutSlowIn);
     animation = new Tween(begin: 0.0, end: 300.0).animate(animationController)
       ..addListener(() {
         print('1变');
-      })
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          animationController.reverse();
-          print('2变');
-        } else if (status == AnimationStatus.dismissed) {
-          animationController.forward();
-          print('3变');
+        if(rotateSign){
+          offsetStartX += 12.0;
+          setState(() {
+            _offset = Offset(offsetStartX, 0);
+          });
+          if (offsetStartX >= 313) {
+            setState(() {
+              _offset = Offset(313, 0);
+              rotateSign = false;
+            });
+            print('stop');
+            animationController.stop();
+          }
+        }else{
+          offsetStartX -= 12.0;
+          setState(() {
+            _offset = Offset(offsetStartX, 0);
+          });
+          if (offsetStartX <= 0) {
+            print('stop');
+            setState(() {
+              _offset = Offset(0, 0);
+              rotateSign = true;
+            });
+            animationController.stop();
+          }
         }
       });
-    animationController.forward();
+//      ..addStatusListener((status) {
+//        if (status == AnimationStatus.completed) {
+////          animationController.reverse();
+//          print('2变');
+//        } else if (status == AnimationStatus.dismissed) {
+////          animationController.forward();
+//          print('3变');
+//        }
+//      });
+//    animationController.forward();
 //    animationController.repeat();
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: Container(
-        color: black,
-        child: Swiper(
-          itemBuilder: (BuildContext context, int index) {
-            return Column(
-              children: <Widget>[
-                rowMaker(),
-              ],
-            );
-          },
-          itemCount: 3,
-          pagination: new SwiperPagination(),
-          control: new SwiperControl(),
+        appBar: new AppBar(
+          title: new Text(widget.title),
         ),
-      )
-    );
+        body: Container(
+          color: black,
+          child: Swiper(
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: <Widget>[
+                  rowMaker(),
+                  rowMaker(),
+                  rowMaker(),
+                ],
+              );
+            },
+            loop: false,
+            itemCount: 3,
+            pagination: new SwiperPagination(),
+            control: new SwiperControl(),
+          ),
+        ));
   }
 
   void dispose() {
@@ -70,13 +102,13 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
     super.dispose();
   }
 
-  rowItemMaker(){
+  Widget rowItemMaker() {
     return Expanded(
         flex: 1,
         child: Container(
           color: black,
           child: Transform(
-            // Transform widget
+              // Transform widget
               transform: Matrix4.identity() //生成一个单位矩阵
                 ..setEntry(3, 2, 0.001) // 透视
                 ..rotateX(0.01 * _offset.dy) // changed
@@ -85,20 +117,34 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
               child: GestureDetector(
 //                onPanUpdate: (details) =>
 //                    setState(() => _offset += details.delta), //与屏幕接触并移动的指针再次移动
-                onDoubleTap: () => setState(() => _offset = Offset.zero),
-//                onDoubleTap: () => Navigator.pushNamed(context, '/animation'),
-                onTap: () => setState(() => _offset = Offset(300.0,0.0)),
-                child: Image.network("http://via.placeholder.com/350x150",
-                    fit: BoxFit.fill),
-              )
-          ),
-        )
-    );
+//                onDoubleTap: () => setState(() => _offset = Offset.zero),
+                onDoubleTap: () => Navigator.pushNamed(context, '/perspective'),
+                onTap: () => animationController.repeat(),
+                child: Stack(
+//                  alignment: const Alignment(0, -0.8),
+                  children: <Widget>[
+                    Container(
+                      child: Image.network("http://via.placeholder.com/350x450",
+                          fit: BoxFit.fill),
+                    ),
+                    Container(
+                      child: Transform(
+                        transform: Matrix4.identity() //生成一个单位矩阵
+//                          ..setEntry(3, 2, 0.001) // 透视
+                          ..rotateY(-math.pi), // changed
+                        alignment: FractionalOffset.center,
+                        child: Text('这个是背面'),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+        ));
   }
 
-  rowMaker(){
+  Widget rowMaker () {
     List<Widget> rowsList = List();
-    for(int i = 0;i < 3;i++){
+    for (int i = 0; i < 3; i++) {
       rowsList.add(rowItemMaker());
     }
     return Row(
